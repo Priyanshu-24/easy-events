ad521937410e01c51e173229910b2ce2c70aee04
priyanshu <priyanshu24pandey@gmail.com>
2023-11-26 16:26:57 +0530

    updated gradient of background

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="h-screen flex justify-center items-center bg-gray-200">
+    <div className="h-screen flex justify-center items-center bg-gradient-to-br from-[#1d3557] to-[#457b9d]">
       <SignIn />
     </div>
   );
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="h-screen flex justify-center items-center bg-gradient-to-r from-[#1d3557] to-[#457b9d]">
+    <div className="h-screen flex justify-center items-center bg-gradient-to-br from-[#1d3557] to-[#457b9d]">
       <SignUp />
     </div>
   );
