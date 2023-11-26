eab899b81adc71a6e8cf6ebdcba7a32cb8e24de7
priyanshu <priyanshu24pandey@gmail.com>
2023-11-26 18:39:28 +0530

    connected mongoDB to the app and made user model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
+import { connectDB } from "@/config/dbConfig";
 
-export default function Home() {
+connectDB();
+
+const Home = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
-}
+};
+
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,15 @@
-import type { Config } from 'tailwindcss';
-import { nextui } from '@nextui-org/react';
+import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/providers/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/providers/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {},
-  plugins: [nextui()]
-}
+  plugins: [nextui()],
+};
 export default config;
